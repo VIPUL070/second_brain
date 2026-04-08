f504919873dec0df9f1806fa589f1e168add1ad9
VIPUL070 <sutharvipul007@gmail.com>
2026-04-08 12:31:06 +0530

    Creating Schemas for the user, content, tags and links for our second brain app

---
+++ src/db.ts
@@ -0,0 +1,74 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+
+dotenv.config();
+const URL = process.env.MONGO_URL;
+
+mongoose.connect(`${URL}/brainly`);
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+const contentTypes = ['image', 'video', 'article', 'audio'];
+
+const contentSchema = new mongoose.Schema({
+    link: {
+        type: String,
+        reuired: true
+    },
+    type: {
+        type: String,
+        enum: contentTypes,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    tags: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Tag",
+        required: true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        require: true
+    }
+
+});
+
+const tagSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    }
+});
+
+const linkSchema = new mongoose.Schema({
+    hash: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        require: true
+    }
+});
+
+export const User = mongoose.model("User", userSchema);
+export const Content = mongoose.model("Content", contentSchema);
+export const Tag = mongoose.model("Tag", tagSchema);
+export const Link = mongoose.model("Link", linkSchema);
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 import express from "express"
+import cors from 'cors'
 
 const app = express();
+app.use(express());
+app.use(cors());
 
+const PORT = 3000;
+app.listen(PORT , ()=> {
+    console.log(`App listening on port: ${PORT}`)
+})
