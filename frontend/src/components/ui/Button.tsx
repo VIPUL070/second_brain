9e2c2390696eda68d991a9b65aef0a9f5e0ab9cd
VIPUL070 <sutharvipul007@gmail.com>
2026-04-10 12:13:28 +0530

    Building our own UI lib/ button component

--- frontend/src/components/ui/Button.tsx
+++ frontend/src/components/ui/Button.tsx
@@ -1,23 +1,41 @@
-import type {ReactNode } from "react";
+import type { ReactNode } from "react";
 
 export interface ButtonProps {
-  variant: "primary" | "secondary",
-  size: "sm" | "md" | "lg",
-  text: string,
-  startIcon?: ReactNode,
-  endIcon?: ReactNode,
-  onClick?: () => void
-
+  variant: "primary" | "secondary";
+  size: "sm" | "md" | "lg";
+  title: string;
+  startIcon?: ReactNode;
+  endIcon?: ReactNode;
+  onClick?: () => void;
 }
+
+const variantStyles = {
+  primary: "bg-purple text-black",
+  secondary: "bg-purple-dark text-primary-font",
+};
+
+const sizeStyles = {
+  sm: "py-1 px-1 text-sm",
+  md: "py-2 px-4 text-md",
+  lg: "py-3 px-5 text-lg",
+};
+
+const defaultStyles = "rounded-md cursor-pointer";
 
 const Button = (props: ButtonProps) => {
-  const {variant,size,text,startIcon,onClick} = props;
+  const { variant, size, title, startIcon, endIcon } = props;
 
   return (
-    <button  onClick={onClick}>
-      {text}
+    <button
+      className={`${variantStyles[variant]} ${defaultStyles} ${sizeStyles[size]}`}
+    >
+      <div className="flex items-center">
+        {startIcon ? <div>{startIcon}</div> : null}
+        <div className="pl-2 pr-2">{title}</div>
+        {endIcon ? <div>{endIcon}</div> : null}
+      </div>
     </button>
-  )
-}
+  );
+};
 
 export default Button;
---
+++ frontend/src/icons/PlusIcon.tsx
@@ -0,0 +1,24 @@
+import { iconSizeVariants, type IconProps } from "./icon";
+
+const PlusIcon = (props: IconProps) => {
+  return (
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        className={iconSizeVariants[props.size ||"sm"]}
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M12 4.5v15m7.5-7.5h-15"
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default PlusIcon;
